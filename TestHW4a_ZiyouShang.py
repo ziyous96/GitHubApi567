4161f5f95e13b5990c806a7c2cb3cf60eb7e8c38
ziyous96 <ziyous96@vt.edu>
2020-02-20 00:01:18 -0500

    Add files via upload

--- TestHW4a_ZiyouShang.py
+++ TestHW4a_ZiyouShang.py
@@ -8,8 +8,27 @@ from HW4a_ZiyouShang import get_repo, get_commits
 class TestHW4a(unittest.TestCase):
     """ Test cases for HW4a """
 
-    def test_something(self):
-        self.assertEqual(True, True)
+    def test_correct_result(self):
+        """ test case for checking results of valid user name """
+
+        repos = get_repo("richkempinski")
+        self.assertEqual(len(repos), 5)
+        self.assertEqual(repos[0], "hellogitworld")
+        self.assertEqual(repos[1], "helloworld")
+        self.assertEqual(repos[2], "Mocks")
+        self.assertEqual(repos[3], "Project1")
+        self.assertEqual(repos[4], "threads-of-life")
+
+        self.assertEqual(get_commits("richkempinski", repos[0]), 30)
+        self.assertEqual(get_commits("richkempinski", repos[1]), 6)
+        self.assertEqual(get_commits("richkempinski", repos[2]), 10)
+        self.assertEqual(get_commits("richkempinski", repos[3]), 2)
+        self.assertEqual(get_commits("richkempinski", repos[4]), 1)
+
+    def test_special_cases(self):
+        """ Test cases for invalid input """
+
+        self.assertEqual(get_repo("kkk"), [])
 
 
 if __name__ == '__main__':
